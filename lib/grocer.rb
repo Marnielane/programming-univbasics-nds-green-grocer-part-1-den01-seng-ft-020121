5855a30ee39b3b667dd7cf829c2c29820c30a950
Marnie Robinson <marnielane@gmail.com>
2021-01-21 22:29:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -33,20 +33,8 @@ def consolidate_cart(cart)
       else new_cart[item][:count] = 1 
       end
     end 
-new_cart  
+    new_cart  
+end
 
-end
-  output = {}
-  cart.each do |item|
-    item_name = item.keys[0]
-    if output[item_name]
-      output[item_name][:count] += 1 
-    else
-      output[item_name] = item[item_name]
-      output[item_name][:count] = 1 
-    end
-  end
-  output
-end
 
   
