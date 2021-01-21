35e0389184ac5ce4bfe987e985dc17f5e0773119
Marnie Robinson <marnielane@gmail.com>
2021-01-21 21:01:13 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,6 +1,19 @@
+grocery_shelf = [
+  { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
+  { :item => "CANNED CORN", :price => 2.50, :clearance => false },
+  { :item => "SALSA", :price => 1.50, :clearance => false },
+  { :item => "TORTILLAS", :price => 2.00, :clearance => false },
+  { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
+]
+
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
-  #
+  new hash = {}
+  collection.map { |key, value| 
+    value[name] == String
+    
+  }
   # Consult README for inputs and outputs
 
 end
