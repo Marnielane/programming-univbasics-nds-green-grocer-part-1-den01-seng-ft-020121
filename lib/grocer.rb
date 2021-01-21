e8e3b0503977090e4ca1aba4eff31419522f20a6
Marnie Robinson <marnielane@gmail.com>
2021-01-21 21:24:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,7 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   collection.each { |row, value| 
     row[value].include?(name)
-    collection[row]
+    return collection[row]
   }
   # Consult README for inputs and outputs
 
